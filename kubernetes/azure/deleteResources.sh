c86c6d97b6a54795bcbbf40aca2be77a89b78db9
Well5a <marcelfrederik@aol.de>
2021-01-27 16:40:46 +0100

    cleanup and added configmaps

--- kubernetes/azure/deleteResources.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Delete the ressource group and all associated ressources
-az group delete --name mwe-monitoring
-
-# Delete the cluster information from kubectl
-kubectl config unset users.clusterUser_mwe-monitoring_monitoring-cluster
-kubectl config unset contexts.monitoring-cluster
-kubectl config unset clusters.monitoring-cluster
